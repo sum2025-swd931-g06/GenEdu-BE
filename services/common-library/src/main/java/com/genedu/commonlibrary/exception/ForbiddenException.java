9141e52747bc5f1362efc16f7d17bd928db95ec3
ChauVinhTienIT <vinhtienchau.it@gmail.com>
2025-06-10 15:41:51 +0700

    update multi module pom management
    - add common library
    - update central maven project for common used dependency

---
+++ services/common-library/src/main/java/com/genedu/commonlibrary/exception/ForbiddenException.java
@@ -0,0 +1,16 @@
+package com.genedu.commonlibrary.exception;
+
+import utils.MessagesUtils;
+
+public class ForbiddenException extends RuntimeException {
+    private final String message;
+
+    public ForbiddenException(String errorCode, Object... var2) {
+        this.message = MessagesUtils.getMessage(errorCode, var2);
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
