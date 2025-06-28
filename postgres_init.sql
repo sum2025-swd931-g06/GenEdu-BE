75479e5db2f0e4ea8a0dda1a20a77a7c76200c89
ChauVinhTienIT <vinhtienchau.it@gmail.com>
2025-06-28 14:24:18 +0700

    feat: add notification service configuration and update related files
    Signed-off-by: ChauVinhTienIT <vinhtienchau.it@gmail.com>

--- postgres_init.sql
+++ postgres_init.sql
@@ -17,3 +17,7 @@ CREATE DATABASE mediadb WITH OWNER = admin ENCODING = 'UTF8' LC_COLLATE = 'en_US
 -- Create Curriculum Service DB
 DROP DATABASE IF EXISTS subscriptiondb;
 CREATE DATABASE subscriptiondb WITH OWNER = admin ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+
+-- Create Curriculum Service DB
+DROP DATABASE IF EXISTS notificationdb;
+CREATE DATABASE notificationdb WITH OWNER = admin ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
